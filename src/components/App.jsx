33314cc931a18170cbfeb5344a5847273873ae46
Elvis W. Vasquez <wes9319_rhcp@hotmail.com>
2022-04-26 19:29:58 -0500

    Initial state

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/components/app.css'
+;
+
+const App = () => {
+  return (
+    <h1>App</h1>
+  )
+}
+
+export { App };
---
+++ src/routes/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../containers/Home";
+import { Checkout } from "../containers/Checkout";
+import { Information } from "../containers/Information";
+import { Payment } from "../containers/Payment";
+import { Success } from "../containers/Success";
+import { NotFound } from "../containers/NotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/checkout" element={<Checkout />}/>
+        <Route exact path="/checkout/information" element={<Information />}/>
+        <Route exact path="/checkout/payment" element={<Payment />}/>
+        <Route exact path="/checkout/success" element={<Success />}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export { App };
