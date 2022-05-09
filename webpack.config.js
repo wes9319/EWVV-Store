3202f15cc135b32b54735e419bce8ba9ac7b35a9
Elvis W. Vasquez <wes9319_rhcp@hotmail.com>
2022-05-09 12:21:46 -0500

    [FEAT] PWA config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -51,6 +52,13 @@ module.exports = {
       filename: 'assets/[name].css',
     }),
     new Dotenv(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: '' },
+        { from: 'public/service-worker.js', to: '' },
+        { from: 'public/icon.png', to: 'assets' },
+      ],
+    }),
   ],
   devServer: {
     static: path.join(__dirname, 'dist'),
