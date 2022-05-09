2304af9ad92faabf85ef47d9254d3d776962bea7
Elvis W. Vasquez <wes9319_rhcp@hotmail.com>
2022-05-09 11:29:37 -0500

    [ADD] react helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { sumTotal } from '../utils/sumTotal';
@@ -13,30 +14,35 @@ function Checkout() {
   }
 
   return (
-   <div className="Checkout">
-     <div className="Checkout-content">
-       <h3>{cart.length > 0 ? 'Items List' : 'No Items'}</h3>
-       {cart.map((item) => (
-        <div className="Checkout-item" key={item.title}>
-          <div className="Checkout-element">
-            <h4>{item.title}</h4>
-            <span>$ {item.price}</span>
-          </div>
-          <button type='button' onClick={handleRemove(item)}>
-            <i className='fas fa-trash-alt' title='Delete Item'/>
-          </button>
+    <>
+    <Helmet>
+      <title>Product list - EWVV Store</title>
+    </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          <h3>{cart.length > 0 ? 'Items List' : 'No Items'}</h3>
+          {cart.map((item) => (
+            <div className="Checkout-item" key={item.title}>
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>$ {item.price}</span>
+              </div>
+              <button type='button' onClick={handleRemove(item)}>
+                <i className='fas fa-trash-alt' title='Delete Item'/>
+              </button>
+            </div>
+          ))}
         </div>
-       ))}
-     </div>
-     {cart.length > 0 && (
-      <div className="Checkout-sidebar">
-        <h3>{`Total: $ ${sumTotal(cart)}`}</h3>
-        <Link to='/checkout/information'>
-          <button type='button'> Checkout </button>
-        </Link>
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Total: $ ${sumTotal(cart)}`}</h3>
+            <Link to='/checkout/information'>
+              <button type='button'> Checkout </button>
+            </Link>
+          </div>
+        )}
       </div>
-     )}
-   </div>
+    </>
   )
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import initialState from '../initialState';
 import { Products } from '../components/Products';
 
 const Home = () => {
   return (
-    <Products products={initialState.products}/>
+    <>
+      <Helmet>
+        <title>EWVV Store - Products</title>
+      </Helmet>
+      <Products products={initialState.products}/>
+    </>
   )
 }
 
